c7d822f2129aa3d7b3af65ec8bcce3246768c5f7
Niklas <116020730+NiklasW00@users.noreply.github.com>
2023-09-15 10:52:30 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,17 @@
-import.java.Scanner
+package QuizGame;
+import java.util.Scanner;
+
+
+
 public class Main {
-   public static void main(String[] args) {
-     Scanner input = new Scanner(System.in);
-		int totalScore = 0;
-      
-   }
+	// starts with 0 score and Input
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int totalScore = 0;
+        //Get Questions and Answers from the other files
+        Questions obj = new Questions(input);
+        Answers obj2 = new Answers(input, totalScore);
+        // Print out score
+        System.out.println("Final score: " + totalScore);
+    }
 }
