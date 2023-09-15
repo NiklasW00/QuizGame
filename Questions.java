bfc1aac209e8597e7acf923762dca02219ae58d7
Niklas <116020730+NiklasW00@users.noreply.github.com>
2023-09-15 10:55:08 +0200

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,5 +1,19 @@
+package QuizGame;
+import java.util.Scanner;
+
 public class Questions {
-
-
-	
+    private String[] questions = {
+    		// Questions added in order
+        "Question 1: In which year was Star Wars Episode 4 released?",
+        "Question 2: What was the first videogame?",
+        "Question 3: Who plays the lead guitar in AC/DC?"
+    };
+    //print out questions in order and make --- between
+    public Questions(Scanner input) {
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            System.out.println("---");
+        }
+    }
+    
 }
