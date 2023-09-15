8f33ab02687de2559d390b7889a9bb6f2dd99d91
yueeeZh <145097799+yueeeZh@users.noreply.github.com>
2023-09-15 19:15:24 +0200

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,8 +1,14 @@
 public class Questions {
 
-	public static void main(String[] args) {
-		
-		test;
-	}
+	public String [] questions;
+	public Questions() {
+		questions = new String [] {"what's your name?","How are you?","where are you from?"};
 	
+	}	
+	public String getQuestion(int index) {
+		if (index>=0 && index <questions.length) {
+			return questions[index];
+		}
+		return null;
+	}
 }
