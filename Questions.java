101211b4090656bcc2af7173b1121c5d76f7553b
ZhuoyingSun <144898476+ZhuoyingSun@users.noreply.github.com>
2023-09-16 00:35:04 +0800

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -5,7 +5,7 @@ public class Questions {
         questions = new String[]{
             "Question 1: What is the capital of China?",
             "Question 2: Which state in Germany is Technische Universitaet Clausthal located?",
-            "Question 3: What is the capital of China?",
+            "Question 3: What time does class start every day?",
         };
     }
 
