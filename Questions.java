c7549982a77094416f87186646b4eb69e3232e83
ZhuoyingSun <144898476+ZhuoyingSun@users.noreply.github.com>
2023-09-16 00:31:36 +0800

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -1,8 +1,16 @@
 public class Questions {
+    private String[] questions;
 
-	public static void main(String[] args) {
-		
-		
-	}
-	
+    public Questions() {
+        questions = new String[]{
+            "Question 1: What is the capital of China?",
+            "Question 2: Which state in Germany is Technische Universitaet Clausthal located?",
+            "Question 3: What is the capital of China?",
+        };
+    }
+
+    public String[] getQuestions() {
+        return questions;
+    }
 }
+
