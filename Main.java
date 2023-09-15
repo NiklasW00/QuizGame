be41af06b6260b453d810616d136964887dd263c
yueeeZh <145097799+yueeeZh@users.noreply.github.com>
2023-09-15 19:16:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
    public static void main(String[] args) {
-     
+	   int questionNumber = 1;
+       Questions questionBank = new Questions();
+       Answers answerBank = new Answers();
+       String question = questionBank.getQuestion(questionNumber);
+       String answer = answerBank.getAnswer(questionNumber);
+       System.out.println(question);
+       System.out.println(answer);
    }
 }
+
